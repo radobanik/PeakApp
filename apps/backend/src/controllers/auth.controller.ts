075e85d770d2d88c262f830bba1ecb6a59d30951
Radoslav Banik <banik@amsyscz.cz>
2025-03-30 21:37:54 +0200

    PA-31&PA-33: add register logic

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, RequestHandler } from "express";
 import jwt from "jsonwebtoken";
 import { HTTP_STATUS } from "./utils/httpStatusCodes";
 import UserRepository from "../repositories/user.repository";
+import { UserCreate, userCreateValidate } from "../model/user/index";
 
 const login: RequestHandler = async (req, res) => {
     const { username, password } = req.body;
@@ -35,6 +36,32 @@ const login: RequestHandler = async (req, res) => {
     });
 };
 
+const register: RequestHandler = async (req, res) => {
+    const userData: UserCreate = req.body;
+
+    const validationResult = userCreateValidate(userData);
+    if (validationResult.error) {
+        res.status(HTTP_STATUS.BAD_REQUEST_400).json({
+            error: "Invalid user data",
+            details: validationResult.error.errors.map((err) => ({
+                field: err.path.join("."),
+                message: err.message,
+            })),
+        });
+        return;
+    }
+
+    try {
+        const user = await UserRepository.createUser(validationResult.data);
+        res.status(HTTP_STATUS.CREATED_201).json(user);
+    } catch (error) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR_500).json({
+            error: "Failed to register user",
+        });
+    }
+};
+
 export default {
-    login
+    login,
+    register,
 };
