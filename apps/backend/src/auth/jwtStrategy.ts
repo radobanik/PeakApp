d4ccff0dfa5ed03954950f371a8bf0c4db4a0f10
Radoslav Banik <banik@amsyscz.cz>
2025-03-30 21:09:57 +0200

    PA-31&PA-33: auth setup

---
+++ apps/backend/src/auth/jwtStrategy.ts
@@ -0,0 +1,31 @@
+import { Strategy as JwtStrategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
+import { PassportStatic } from 'passport';
+import userRepository from "../repositories/user.repository";
+
+const opts: StrategyOptions = {
+  jwtFromRequest: (req) => {
+    const token = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+    return token;
+  },
+  secretOrKey: process.env.JWT_SECRET || "defaultsecret",
+};
+
+interface JwtPayload {
+  id: string;
+}
+
+export default (passport: PassportStatic) => {
+  passport.use(
+    new JwtStrategy(opts, async (jwtPayload, done) => {
+      try {
+        const user = await userRepository.getUserById(jwtPayload.id);
+        if (user) 
+          return done(null, user);
+        else 
+          return done(null, false);
+      } catch (err) {
+        return done(err, false);
+      }
+    })
+  );
+};
