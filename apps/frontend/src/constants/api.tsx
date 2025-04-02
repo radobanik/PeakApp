3cab48817e0e64b6234a6340f1be25e3eaa474a3
Radoslav Banik <banik@amsyscz.cz>
2025-04-02 15:02:11 +0200

    PA-30: delete unused logout endpoint

--- apps/frontend/src/constants/api.tsx
+++ apps/frontend/src/constants/api.tsx
@@ -4,7 +4,6 @@ export const API = {
     AUTH: {
         LOGIN: `${API_BASE_URL}/auth/login`,
         REGISTER: `${API_BASE_URL}/auth/register`,
-        LOGOUT: `${API_BASE_URL}/auth/logout`,
     },
     // Example of API endpoints for user. Need to be updated based on real API 
     USER: {
