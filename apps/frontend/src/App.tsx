2fa3c7da45ea35b42bfc67dd568116fe6450c1be
Radoslav Banik <banik@amsyscz.cz>
2025-04-02 13:32:42 +0200

    PA-30: add public routing

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,12 +3,13 @@ import LoginPage from "@/pages/LoginPage";
 import HomePage from "@/pages/HomePage";
 import { ROUTE } from "@/constants/routes";
 import { privateRoute } from "@/routing/privateRoute";
+import { publicRoute } from "@/routing/publicRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={ROUTE.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTE.LOGIN} element={publicRoute(<LoginPage />)} />
         <Route path={ROUTE.HOME}  element={privateRoute(<HomePage />)} />
       </Routes>
     </BrowserRouter>
