97570231ffe505d649d32bd509d3267a0ac0cdaa
Radoslav Banik <banik@amsyscz.cz>
2025-04-02 14:58:06 +0200

    PA-30: fix api constants users typo + add example comment

--- apps/frontend/src/constants/api.tsx
+++ apps/frontend/src/constants/api.tsx
@@ -6,8 +6,9 @@ export const API = {
         REGISTER: `${API_BASE_URL}/auth/register`,
         LOGOUT: `${API_BASE_URL}/auth/logout`,
     },
+    // Example of API endpoints for user. Need to be updated based on real API 
     USER: {
-        PROFILE: `${API_BASE_URL}/users/profile`,
-        UPDATE: `${API_BASE_URL}/users/update`,
+        PROFILE: `${API_BASE_URL}/user/profile`,
+        UPDATE: `${API_BASE_URL}/user/update`,
     }
 };
