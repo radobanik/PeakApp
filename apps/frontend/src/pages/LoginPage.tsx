53747d4751b847b7b2ea2715630297f0d41ef236
Radoslav Banik <banik@amsyscz.cz>
2025-04-02 12:25:03 +0200

    PA-30: add login page

---
+++ apps/frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import { LoginForm } from "@/components/LoginForm";
+import PeakAppLogo from '../assets/PeakAppLogo.png';
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-accent px-4">
+      <div className="w-full max-w-md">
+        <img src={PeakAppLogo} alt="Logo" className="h-16 mx-auto mb-6" />
+        <LoginForm className="w-full" />
+      </div>
+    </div>
+  );
+}
