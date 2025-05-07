0e4ecceb1312b26a5e2ae3ac20627384d1e0b3f1
Matúš Fedorko <xfedork1@fi.muni.cz>
2025-05-08 01:23:34 +0200

    pa76: controller

---
+++ apps/backend/src/controllers/comment.controller.ts
@@ -0,0 +1,123 @@
+import { Request, Response } from 'express'
+import { HTTP_STATUS } from './utils/httpStatusCodes'
+import { CommentRepository, SessionRepository, UserRepository } from '../repositories'
+import { defaultCommentListParams, IncommingCommentListParams } from '../model/comment/commentList'
+import { provideUserRefFromToken } from '../auth/authUtils'
+import { CommentCreate, commentUpdateValidate, commentCreateValidate } from '../model/comment'
+import requestValidator from '../model/common/validator'
+import { Role } from '@prisma/client'
+import { RefObject } from '../model/common/refObject'
+
+const validateUser = async (
+  userRef: RefObject,
+  commentUserId: string,
+  res: Response
+): Promise<boolean> => {
+  const user = await UserRepository.getUserById(userRef.id)
+
+  if (commentUserId !== user!.id && !user!.roles.includes(Role.ADMIN)) {
+    res.status(HTTP_STATUS.FORBIDDEN_403).json({ error: 'Forbidden' })
+    return false
+  }
+
+  return true
+}
+
+const listBySession = async (req: Request, res: Response) => {
+  const params = defaultCommentListParams(req.query as unknown as IncommingCommentListParams)
+  const sessionId = params.sessionId
+
+  if (sessionId === null) {
+    res.status(HTTP_STATUS.BAD_REQUEST_400).json({ error: 'Session ID is required' })
+    return
+  }
+
+  const sessionExists = await SessionRepository.existsId(sessionId)
+  if (!sessionExists) {
+    res.status(HTTP_STATUS.NOT_FOUND_404).json({ error: 'Session not found' })
+    return
+  }
+
+  const comments = await CommentRepository.listBySession(
+    sessionId,
+    params.cursorId,
+    params.pageSize
+  )
+  res.status(HTTP_STATUS.OK_200).json(comments)
+}
+
+const create = async (req: Request, res: Response) => {
+  const userRef = provideUserRefFromToken(req)
+  if (userRef === null) {
+    res.status(HTTP_STATUS.UNAUTHORIZED_401).json({ error: 'Unauthorized' })
+    return
+  }
+
+  const data: CommentCreate = req.body
+  const validatedData = requestValidator(() => commentCreateValidate(data), res)
+  if (!validatedData) return
+
+  const sessionExists = await SessionRepository.existsId(data.session.id)
+  if (!sessionExists) {
+    res.status(HTTP_STATUS.NOT_FOUND_404).json({ error: 'Session not found' })
+    return
+  }
+
+  const createdComment = await CommentRepository.create(data, userRef)
+  res.status(HTTP_STATUS.CREATED_201).json(createdComment)
+}
+
+const update = async (req: Request, res: Response) => {
+  const userRef = provideUserRefFromToken(req)
+  if (userRef === null) {
+    res.status(HTTP_STATUS.UNAUTHORIZED_401).json({ error: 'Unauthorized' })
+    return
+  }
+
+  const commentId = req.params.id
+  const data: CommentCreate = req.body
+  const validatedData = requestValidator(() => commentUpdateValidate(data), res)
+  if (!validatedData) return
+
+  const comment = await CommentRepository.getById(commentId)
+  if (!comment) {
+    res.status(HTTP_STATUS.NOT_FOUND_404).json({ error: 'Comment not found' })
+    return
+  }
+
+  if (!validateUser(userRef, comment.user.id, res)) {
+    return
+  }
+
+  const updatedComment = await CommentRepository.update(data, commentId)
+  res.status(HTTP_STATUS.OK_200).json(updatedComment)
+}
+
+const deleteById = async (req: Request, res: Response) => {
+  const commentId = req.params.id
+  const userRef = provideUserRefFromToken(req)
+  if (userRef === null) {
+    res.status(HTTP_STATUS.UNAUTHORIZED_401).json({ error: 'Unauthorized' })
+    return
+  }
+
+  const comment = await CommentRepository.getById(commentId)
+  if (!comment) {
+    res.status(HTTP_STATUS.NOT_FOUND_404).json({ error: 'Comment not found' })
+    return
+  }
+
+  if (!validateUser(userRef, comment.user.id, res)) {
+    return
+  }
+
+  await CommentRepository.deleteById(commentId)
+  res.status(HTTP_STATUS.NO_CONTENT_204).send()
+}
+
+export default {
+  listBySession,
+  create,
+  update,
+  deleteById,
+}
