f3c714e532de488764fd8523dcd60e6b59bda755
Radoslav Banik <banik@amsyscz.cz>
2025-05-17 12:18:06 +0200

    PA83 - notif repo

---
+++ apps/backend/src/repositories/notification.repository.ts
@@ -0,0 +1,90 @@
+import { PrismaClient } from '@prisma/client'
+import {
+  Notification,
+  NotificationCreate,
+  NotificationUpdate,
+  NotificationList,
+  notificationListSelector,
+  notificationSelector,
+} from '../model/notification'
+
+const notificationClient = new PrismaClient().notification
+
+const listByUser = async (userId: string): Promise<NotificationList[]> => {
+  return await notificationClient.findMany({
+    where: { userId },
+    orderBy: { createdAt: 'desc' },
+    select: notificationListSelector,
+  })
+}
+
+const getById = async (id: string): Promise<Notification | null> => {
+  return await notificationClient.findUnique({
+    where: { id },
+    select: notificationSelector,
+  })
+}
+
+const create = async (data: NotificationCreate): Promise<Notification> => {
+  return await notificationClient.create({
+    data,
+    select: notificationSelector,
+  })
+}
+
+const update = async (id: string, data: NotificationUpdate): Promise<Notification> => {
+  return await notificationClient.update({
+    where: { id },
+    data,
+    select: notificationSelector,
+  })
+}
+
+const deleteById = async (id: string): Promise<void> => {
+  await notificationClient.delete({
+    where: { id },
+  })
+}
+
+const markAsRead = async (id: string): Promise<Notification> => {
+  return await notificationClient.update({
+    where: { id },
+    data: { isRead: true },
+    select: notificationSelector,
+  })
+}
+
+const listAndMarkAllAsRead = async (userId: string): Promise<NotificationList[]> => {
+  await notificationClient.updateMany({
+    where: { userId, isRead: false },
+    data: { isRead: true },
+  })
+
+  return await notificationClient.findMany({
+    where: { userId },
+    orderBy: { createdAt: 'desc' },
+    select: notificationListSelector,
+  })
+}
+
+const countUnreadByUser = async (userId: string): Promise<number> => {
+  const count = await notificationClient.count({
+    where: {
+      userId,
+      isRead: false,
+    },
+  })
+
+  return count
+}
+
+export default {
+  listByUser,
+  getById,
+  create,
+  update,
+  deleteById,
+  markAsRead,
+  listAndMarkAllAsRead,
+  countUnreadByUser,
+}
