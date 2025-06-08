97566b30f5f327ea930af21d46411ea0ee08d79a
Adam Haluska <xhaluska@fi.muni.cz>
2025-06-08 15:27:33 +0200

    PA-101: Fix non-working edit in ActivityDetail

--- apps/frontend/src/components/EntityDetailHeader.tsx
+++ apps/frontend/src/components/EntityDetailHeader.tsx
@@ -2,6 +2,8 @@ import { useContext } from 'react'
 import BackButon from './BackButton'
 import { EntityOptionsDropdown } from './ui/custom/entity-option-dropdown'
 import { SessionDetailContext } from '@/pages/SessionDetailPage'
+import { ActivityDetailContext } from '@/pages/ActivityDetailPage'
+import { ROUTE } from '@/constants/routes'
 
 type EntityDetailHeaderProps = {
   backRoute: string
@@ -9,11 +11,26 @@ type EntityDetailHeaderProps = {
 
 export default function EntityDetailHeader({ backRoute }: EntityDetailHeaderProps) {
   const { setIsDeleteDialogOpen, setIsEdit } = useContext(SessionDetailContext)
+  const { setIsDeleteDialogOpen: setIsDeleteDialogOpenActivity, setIsEdit: setIsEditActivity } =
+    useContext(ActivityDetailContext)
 
   return (
     <div className="flex flex-row justify-between p-4">
-      <BackButon backRoute={backRoute} />
-      <EntityOptionsDropdown setDelete={setIsDeleteDialogOpen} setIsEdit={setIsEdit} />
+      {backRoute === ROUTE.SESSIONS && (
+        <>
+          <BackButon backRoute={backRoute} />
+          <EntityOptionsDropdown setDelete={setIsDeleteDialogOpen} setIsEdit={setIsEdit} />
+        </>
+      )}
+      {backRoute === ROUTE.ACTIVITIES && (
+        <>
+          <BackButon backRoute={backRoute} />
+          <EntityOptionsDropdown
+            setDelete={setIsDeleteDialogOpenActivity}
+            setIsEdit={setIsEditActivity}
+          />
+        </>
+      )}
     </div>
   )
 }
