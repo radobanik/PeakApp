d50d011deb6d3deabfe3fb0a5bc46cfe1b3afc52
Adam Haluska <xhaluska@fi.muni.cz>
2025-06-09 01:13:42 +0200

    PA-101: Fix contradicting tailwind parameters

--- apps/frontend/src/components/ExpandableDescription.tsx
+++ apps/frontend/src/components/ExpandableDescription.tsx
@@ -15,7 +15,7 @@ const ExpandableDescription: FC<ExpandableDescriptionProps> = ({
       <p className={cn('text-sm text-muted-foreground', !expanded && `line-clamp-3`)}>{text}</p>
       <button
         onClick={() => setExpanded(!expanded)}
-        className="text-xs font-medium text-black font-bold hover:underline"
+        className="text-xs text-black font-bold hover:underline"
       >
         {expanded ? 'Show less' : 'Show full'}
       </button>
