d61782afc3932781b70de0b07f27ccf4e29bbc4c
Adam Haluska <xhaluska@fi.muni.cz>
2025-06-08 15:28:23 +0200

    PA-101: Change grade formatting on Route detail

--- apps/frontend/src/components/RouteDetailBase.tsx
+++ apps/frontend/src/components/RouteDetailBase.tsx
@@ -2,6 +2,9 @@ import { RouteDetail } from '@/types/routeTypes'
 import { getTextColorForBackground } from '@/lib/utils'
 import { Rating } from '@smastrom/react-rating'
 import '@smastrom/react-rating/style.css'
+import { useReviewsQuery, useUserReviewQuery } from '@/services/queryHooks'
+import { useEffect, useState } from 'react'
+import { QueryClient } from '@tanstack/react-query'
 
 type RouteBaseProps = {
   route: RouteDetail
@@ -10,38 +13,59 @@ type RouteBaseProps = {
 const RouteBase = ({ route }: RouteBaseProps) => {
   const gradeColor = route.grade.color
   const userGradeColor = route.userGradeRating.color
+  const [showUserReview, setShowUserReview] = useState<boolean>(false)
+  const reviews = useReviewsQuery(route.id)
+  const meReview = useUserReviewQuery(route.id)
+  const queryClient = new QueryClient()
+
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: ['review', route.id] })
+    if (reviews.data) {
+      const reviewCount = (meReview.data ? 1 : 0) + reviews.data.items.length
+      setShowUserReview(reviewCount > 0)
+    }
+  }, [reviews.data, meReview.data, route])
 
   return (
     <div className="w-full overflow-hidden">
-      <div className="flex justify-between text-lg font-semibold">
-        <h1 className="text-2xl font-bold">{route.name}</h1>
-        <div className="flex items-center gap-2">
-          <h1>Official: </h1>
-          <span
-            className="text-xl font-semibold px-2 py-1 rounded "
-            style={{ backgroundColor: gradeColor, color: getTextColorForBackground(gradeColor) }}
-          >
-            {route.grade.name}
-          </span>
+      <div className="grid grid-flow-col gap-2 text-lg font-semibold">
+        <div className="col-start-1">
+          <h1 className="text-2xl font-bold">{route.name}</h1>
+          <span className="text-xl font-semibold">{route.climbingStructureType}</span>
         </div>
-      </div>
-      <div className="flex justify-between text-lg font-semibold">
-        <Rating style={{ maxWidth: 140 }} value={route.averageStar} readOnly />
-        <div className="flex items-center gap-2">
-          <h1>Users:</h1>
-          <span
-            className="text-xl font-semibold px-2 py-1 rounded"
-            style={{
-              backgroundColor: userGradeColor,
-              color: getTextColorForBackground(userGradeColor),
-            }}
-          >
-            {route.userGradeRating.name}
-          </span>
+        <div className="col-start-1">
+          <Rating style={{ maxWidth: 140 }} value={route.averageStar} readOnly />
         </div>
-      </div>
-      <div className="flex items-end justify-end mt-2">
-        <span className="text-xl font-semibold">{route.climbingStructureType}</span>
+        <div className="col-start-2 row-span-2 flex flex-row items-center justify-end pr-5 ">
+          <div className="flex flex-col items-center pr-5">
+            <h1>Author</h1>
+            <span
+              className="text-xl font-semibold px-2 py-1 rounded "
+              style={{
+                backgroundColor: gradeColor,
+                color: getTextColorForBackground(gradeColor),
+              }}
+            >
+              {route.grade.name}
+            </span>
+          </div>
+          <div className="flex flex-col items-center">
+            <h1>Users</h1>
+            {showUserReview ? (
+              <span
+                className="text-xl font-semibold px-2 py-1 rounded"
+                style={{
+                  backgroundColor: userGradeColor,
+                  color: getTextColorForBackground(userGradeColor),
+                }}
+              >
+                {route.userGradeRating.name}
+              </span>
+            ) : (
+              <span className="text-xl font-semibold px-2 py-1 rounded">-</span>
+            )}
+          </div>
+        </div>
       </div>
 
       <div className="mt-1 text-base">{route.description}</div>
