d5781ec6ffa11f73df167b74acb70efce6b9c38e
Adam Haluska <xhaluska@fi.muni.cz>
2025-06-09 02:51:17 +0200

    PA-101: Add images to init-data cmd

--- apps/backend/src/init/route.init.ts
+++ apps/backend/src/init/route.init.ts
@@ -1,5 +1,5 @@
 import prisma from '../core/prisma/client'
-import { ApprovalState, ClimbingStructureType } from '@prisma/client'
+import { ApprovalState, ClimbingStructureType, PeakFileSource } from '@prisma/client'
 import * as UserInit from './user.init'
 import * as GradeInit from './grade.init'
 import * as ClimbingObjectInit from './climbingObject.init'
@@ -35,6 +35,322 @@ export const ROUTE_28_ID = '766d1b3d-cdce-4689-b876-80ab909ad72e'
 export const ROUTE_29_ID = '571fa7c0-2825-4e40-ae29-88f2e62cb16d'
 export const ROUTE_30_ID = '38184bc0-6dcb-4134-8f88-2784eab99fc9'
 
+export const ROUTE_IMG_1 = '7c479473-2e6f-4719-9225-9b5e3f8593cf'
+export const ROUTE_IMG_2 = 'f177314a-ff64-4107-a0f8-371d2226d28a'
+export const ROUTE_IMG_3 = '00f2b59d-c319-4504-91f4-641d583c1f5b'
+export const ROUTE_IMG_4 = '167903db-b349-44ef-bfca-7b6ba6e43bb2'
+export const ROUTE_IMG_5 = '47f8d33b-918d-4382-a6d2-6a3302c1efd5'
+export const ROUTE_IMG_6 = '1d374770-4e92-47a3-a1cf-6f646dc4e34b'
+export const ROUTE_IMG_7 = 'ab9333b3-74f6-48b6-892c-b2ec2dbf46e7'
+export const ROUTE_IMG_8 = 'adc207c2-a7db-42a4-b96d-73b357dda2ce'
+export const ROUTE_IMG_9 = 'f9be05d1-78c3-4827-ad4c-d92ae30cbf19'
+export const ROUTE_IMG_10 = '67415a08-0bd2-4381-b705-b8f6dc375480'
+export const ROUTE_IMG_11 = '9a0d250c-8051-4816-9350-f47d9a6a3cd4'
+export const ROUTE_IMG_12 = '80044085-d35f-47d2-8e2d-ec665a7b09a0'
+export const ROUTE_IMG_13 = 'ede28bbd-e1fb-44e0-8082-50579d500e45'
+export const ROUTE_IMG_14 = 'ed02ef3e-65cf-4b49-9b31-a15088ac1f11'
+export const ROUTE_IMG_15 = '36be2f55-abc5-4e3c-8ec7-b4f22df8aee5'
+export const ROUTE_IMG_16 = 'af5f5940-b46e-4db7-8702-abc2a4965910'
+export const ROUTE_IMG_17 = 'b0bfd99d-4495-4c2c-9354-9612e7f5af27'
+export const ROUTE_IMG_18 = '83482523-24b9-4f5f-af1b-978391a2b2f5'
+export const ROUTE_IMG_19 = '8958a865-ffde-4d94-8ffc-0f988302a0b5'
+export const ROUTE_IMG_20 = '4eaef8bf-b11f-4bb0-9ff0-a00d0e90c221'
+export const ROUTE_IMG_21 = '1a98082e-d783-4fbd-9972-c5e572a5eeef'
+export const ROUTE_IMG_22 = '41f7eca2-1fb3-445c-a22e-671fa8101712'
+export const ROUTE_IMG_23 = 'a4aa4285-581c-41b9-bdfd-8d180ad26640'
+export const ROUTE_IMG_24 = '44609eb3-ec50-44e2-9bdf-a8f315e0e833'
+export const ROUTE_IMG_25 = '73506c70-a125-4a7e-aae1-43444dc6f12c'
+export const ROUTE_IMG_26 = 'b18f2a8e-0403-4dc2-8523-28488a3d0d65'
+export const ROUTE_IMG_27 = 'ee3326d8-75ba-4ba6-9569-8ce6c2689c89'
+export const ROUTE_IMG_28 = '0fe90a05-3c0a-48a5-8c54-cadfec472ce2'
+export const ROUTE_IMG_29 = '323239b8-bb77-4576-8665-4ccc94cbf420'
+export const ROUTE_IMG_30 = '4fed3a66-3680-49e6-b5e3-53c5d8f1a5b7'
+
+const INIT_USER_ID = UserInit.USER_JANE_DOE_ID
+const ROUTE_1_PHOTO = 'route1.png'
+const ROUTE_2_PHOTO = 'route2.png'
+const ROUTE_3_PHOTO = 'route3.png'
+const ROUTE_4_PHOTO = 'route4.jpg'
+const ROUTE_5_PHOTO = 'route5.png'
+const ROUTE_6_PHOTO = 'route6.jpg'
+const ROUTE_7_PHOTO = 'route7.jpg'
+const ROUTE_8_PHOTO = 'route8.jpg'
+const ROUTE_9_PHOTO = 'route9.jpg'
+const ROUTE_10_PHOTO = 'route10.jpg'
+
+const photos = [
+  {
+    id: ROUTE_IMG_1,
+    createdAt: new Date(),
+    name: 'route 1 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_1_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_2,
+    createdAt: new Date(),
+    name: 'route 2 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_2_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_3,
+    createdAt: new Date(),
+    name: 'route 3 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_3_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_4,
+    createdAt: new Date(),
+    name: 'route 4 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_4_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_5,
+    createdAt: new Date(),
+    name: 'route 5 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_5_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_6,
+    createdAt: new Date(),
+    name: 'route 6 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_6_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_7,
+    createdAt: new Date(),
+    name: 'route 7 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_7_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_8,
+    createdAt: new Date(),
+    name: 'route 8 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_8_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_9,
+    createdAt: new Date(),
+    name: 'route 9 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_9_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_10,
+    createdAt: new Date(),
+    name: 'route 10 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_10_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_11,
+    createdAt: new Date(),
+    name: 'route 11 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_1_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_12,
+    createdAt: new Date(),
+    name: 'route 12 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_2_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_13,
+    createdAt: new Date(),
+    name: 'route 13 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_3_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_14,
+    createdAt: new Date(),
+    name: 'route 14 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_4_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_15,
+    createdAt: new Date(),
+    name: 'route 15 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_5_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_16,
+    createdAt: new Date(),
+    name: 'route 16 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_6_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_17,
+    createdAt: new Date(),
+    name: 'route 17 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_7_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_18,
+    createdAt: new Date(),
+    name: 'route 18 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_8_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_19,
+    createdAt: new Date(),
+    name: 'route 19 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_9_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_20,
+    createdAt: new Date(),
+    name: 'route 20 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_10_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_21,
+    createdAt: new Date(),
+    name: 'route 21 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_1_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_22,
+    createdAt: new Date(),
+    name: 'route 22 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_2_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_23,
+    createdAt: new Date(),
+    name: 'route 23 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_3_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_24,
+    createdAt: new Date(),
+    name: 'route 24 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_4_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_25,
+    createdAt: new Date(),
+    name: 'route 25 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_5_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_26,
+    createdAt: new Date(),
+    name: 'route 26 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_6_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_27,
+    createdAt: new Date(),
+    name: 'route 27 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_7_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_28,
+    createdAt: new Date(),
+    name: 'route 28 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_8_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_29,
+    createdAt: new Date(),
+    name: 'route 29 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_9_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+  {
+    id: ROUTE_IMG_30,
+    createdAt: new Date(),
+    name: 'route 30 photo',
+    contentType: 'image/jpeg',
+    identifier: ROUTE_10_PHOTO,
+    source: PeakFileSource.S3_BUCKET,
+    createdById: INIT_USER_ID,
+  },
+]
+
 const routes = [
   {
     id: ROUTE_1_ID,
@@ -46,6 +362,7 @@ const routes = [
     longitude: 16.607885,
     latitude: 49.206331,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_1_ID,
+    imageId: ROUTE_IMG_1,
   },
   {
     id: ROUTE_2_ID,
@@ -57,6 +374,7 @@ const routes = [
     longitude: 16.607895,
     latitude: 49.20632,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_1_ID,
+    imageId: ROUTE_IMG_2,
   },
   {
     id: ROUTE_3_ID,
@@ -68,6 +386,7 @@ const routes = [
     longitude: 16.6079,
     latitude: 49.20631,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_1_ID,
+    imageId: ROUTE_IMG_3,
   },
   {
     id: ROUTE_4_ID,
@@ -79,6 +398,7 @@ const routes = [
     longitude: 16.607875,
     latitude: 49.2063,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_1_ID,
+    imageId: ROUTE_IMG_4,
   },
   {
     id: ROUTE_5_ID,
@@ -90,6 +410,7 @@ const routes = [
     longitude: 16.60788,
     latitude: 49.20629,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_1_ID,
+    imageId: ROUTE_IMG_5,
   },
   {
     id: ROUTE_6_ID,
@@ -101,6 +422,7 @@ const routes = [
     longitude: 16.607905,
     latitude: 49.206289,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_1_ID,
+    imageId: ROUTE_IMG_6,
   },
   {
     id: ROUTE_7_ID,
@@ -112,6 +434,7 @@ const routes = [
     longitude: 16.60789,
     latitude: 49.20631,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_1_ID,
+    imageId: ROUTE_IMG_7,
   },
 
   {
@@ -124,6 +447,7 @@ const routes = [
     longitude: 16.600707,
     latitude: 49.239977,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_2_ID,
+    imageId: ROUTE_IMG_8,
   },
   {
     id: ROUTE_9_ID,
@@ -135,6 +459,7 @@ const routes = [
     longitude: 16.60072,
     latitude: 49.23999,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_2_ID,
+    imageId: ROUTE_IMG_9,
   },
   {
     id: ROUTE_10_ID,
@@ -146,6 +471,7 @@ const routes = [
     longitude: 16.60071,
     latitude: 49.239985,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_2_ID,
+    imageId: ROUTE_IMG_10,
   },
   {
     id: ROUTE_11_ID,
@@ -157,6 +483,7 @@ const routes = [
     longitude: 16.60073,
     latitude: 49.23997,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_2_ID,
+    imageId: ROUTE_IMG_11,
   },
   {
     id: ROUTE_12_ID,
@@ -168,6 +495,7 @@ const routes = [
     longitude: 16.600735,
     latitude: 49.23996,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_2_ID,
+    imageId: ROUTE_IMG_12,
   },
 
   {
@@ -180,6 +508,7 @@ const routes = [
     longitude: 16.5955,
     latitude: 49.2455,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_3_ID,
+    imageId: ROUTE_IMG_13,
   },
   {
     id: ROUTE_14_ID,
@@ -191,6 +520,7 @@ const routes = [
     longitude: 16.59549,
     latitude: 49.24552,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_3_ID,
+    imageId: ROUTE_IMG_14,
   },
   {
     id: ROUTE_15_ID,
@@ -202,6 +532,7 @@ const routes = [
     longitude: 16.59548,
     latitude: 49.24553,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_3_ID,
+    imageId: ROUTE_IMG_15,
   },
   {
     id: ROUTE_16_ID,
@@ -213,6 +544,7 @@ const routes = [
     longitude: 16.59547,
     latitude: 49.24551,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_3_ID,
+    imageId: ROUTE_IMG_16,
   },
 
   {
@@ -225,6 +557,7 @@ const routes = [
     longitude: 16.600725,
     latitude: 49.239975,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_4_ID,
+    imageId: ROUTE_IMG_17,
   },
   {
     id: ROUTE_18_ID,
@@ -236,6 +569,7 @@ const routes = [
     longitude: 16.60074,
     latitude: 49.23995,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_4_ID,
+    imageId: ROUTE_IMG_18,
   },
   {
     id: ROUTE_19_ID,
@@ -247,6 +581,7 @@ const routes = [
     longitude: 16.600755,
     latitude: 49.239965,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_4_ID,
+    imageId: ROUTE_IMG_19,
   },
   {
     id: ROUTE_20_ID,
@@ -258,6 +593,7 @@ const routes = [
     longitude: 16.600715,
     latitude: 49.239985,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_4_ID,
+    imageId: ROUTE_IMG_20,
   },
   {
     id: ROUTE_21_ID,
@@ -269,6 +605,7 @@ const routes = [
     longitude: 16.600705,
     latitude: 49.239955,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_4_ID,
+    imageId: ROUTE_IMG_21,
   },
 
   {
@@ -281,6 +618,7 @@ const routes = [
     longitude: 16.59552,
     latitude: 49.24548,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_5_ID,
+    imageId: ROUTE_IMG_22,
   },
   {
     id: ROUTE_23_ID,
@@ -292,6 +630,7 @@ const routes = [
     longitude: 16.59553,
     latitude: 49.24547,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_5_ID,
+    imageId: ROUTE_IMG_23,
   },
   {
     id: ROUTE_24_ID,
@@ -303,6 +642,7 @@ const routes = [
     longitude: 16.595515,
     latitude: 49.24549,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_5_ID,
+    imageId: ROUTE_IMG_24,
   },
   {
     id: ROUTE_25_ID,
@@ -314,6 +654,7 @@ const routes = [
     longitude: 16.595505,
     latitude: 49.2455,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_5_ID,
+    imageId: ROUTE_IMG_25,
   },
   {
     id: ROUTE_26_ID,
@@ -325,6 +666,7 @@ const routes = [
     longitude: 16.59549,
     latitude: 49.24551,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_5_ID,
+    imageId: ROUTE_IMG_26,
   },
 
   {
@@ -337,6 +679,7 @@ const routes = [
     longitude: 16.5821,
     latitude: 49.21045,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_6_ID,
+    imageId: ROUTE_IMG_27,
   },
   {
     id: ROUTE_28_ID,
@@ -348,6 +691,7 @@ const routes = [
     longitude: 16.58212,
     latitude: 49.21046,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_6_ID,
+    imageId: ROUTE_IMG_28,
   },
   {
     id: ROUTE_29_ID,
@@ -359,6 +703,7 @@ const routes = [
     longitude: 16.58211,
     latitude: 49.21047,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_6_ID,
+    imageId: ROUTE_IMG_29,
   },
   {
     id: ROUTE_30_ID,
@@ -370,10 +715,19 @@ const routes = [
     longitude: 16.58209,
     latitude: 49.210455,
     climbingObjectId: ClimbingObjectInit.CLIMBING_OBJECT_6_ID,
+    imageId: ROUTE_IMG_30,
   },
 ]
 
 async function initRoutes() {
+  for (const photo of photos) {
+    await prisma.peakFile.upsert({
+      where: { id: photo.id },
+      update: photo,
+      create: photo,
+    })
+  }
+
   await Promise.all(
     routes.map((route) =>
       prisma.route.upsert({
