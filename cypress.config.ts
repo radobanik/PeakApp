f574c831021f996ab0ac054914d349d4995c79e7
Matúš Jakuboc <xjakuboc@fi.muni.cz>
2025-06-10 00:12:24 +0200

    PA-82: add cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,5 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     supportFile: 'cypress/support/commands.ts',
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
   },
 })
