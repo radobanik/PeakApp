e4bd146997276c66b0d1199caee6d7d286d6313e
Michal Nosal <xnosal1@fi.muni.cz>
2025-06-10 18:12:15 +0200

    fix setting and backoffice slow loading

--- apps/frontend/src/components/SideBarLayout.tsx
+++ apps/frontend/src/components/SideBarLayout.tsx
@@ -14,6 +14,7 @@ import PeakAppLogo from '@/assets/PeakAppLogo.png'
 import { useContext } from 'react'
 import { ViewportContext, useNotificationContext } from '@/App'
 import { ROUTE } from '@/constants/routes'
+import { Link } from 'react-router-dom'
 
 type SidebarItem = {
   title: string
@@ -47,23 +48,21 @@ export function SidebarLayout({ sections, ...props }: SidebarLayoutProps) {
             {sections.map((section) => (
               <SidebarMenuItem key={section.title}>
                 <SidebarMenuButton asChild>
-                  <a href={section.url} className="font-medium">
-                    {section.title}
-                  </a>
+                  <p className="font-medium">{section.title}</p>
                 </SidebarMenuButton>
                 {section.items?.length ? (
                   <SidebarMenuSub className="ml-4 border-l-2 border-sidebar-border pl-2">
                     {section.items.map((subItem) => (
                       <SidebarMenuSubItem key={subItem.title}>
                         <SidebarMenuSubButton asChild isActive={subItem.isActive}>
-                          <a href={subItem.url} className="flex items-center gap-1 relative">
+                          <Link to={subItem.url} className="flex items-center gap-1 relative">
                             <span>{subItem.title}</span>
                             {subItem.url === ROUTE.SETTINGS_NOTIFICATIONS && unreadCount > 0 && (
                               <span className="ml-1 bg-red-500 text-white text-[10px] font-bold rounded-full w-4 h-4 flex items-center justify-center shadow-md translate-y-[2px]">
                                 {unreadCount > 9 ? '9+' : unreadCount}
                               </span>
                             )}
-                          </a>
+                          </Link>
                         </SidebarMenuSubButton>
                       </SidebarMenuSubItem>
                     ))}
